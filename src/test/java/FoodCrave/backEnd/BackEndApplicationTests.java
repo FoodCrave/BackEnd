47c3081bf5f0049d1b70e420a546260886f164f4
LAPTOP-81HUI3ID\78175 <781754008@gmail.com>
2022-02-15 14:57:00 -0800

    initialize springboot

---
+++ src/test/java/FoodCrave/backEnd/BackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package FoodCrave.backEnd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
